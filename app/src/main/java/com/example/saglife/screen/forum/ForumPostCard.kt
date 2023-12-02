8d913bb4d461edd7b963c456335c24fcb059d1ac
ValentineGilles <71833334+ValentineGilles@users.noreply.github.com>
2023-12-02 10:46:00 -0500

    Zoom et rotation photo (mise à l'échelle non fonctionnelle)

--- app/src/main/java/com/example/saglife/screen/forum/ForumPostCard.kt
+++ app/src/main/java/com/example/saglife/screen/forum/ForumPostCard.kt
@@ -2,21 +2,31 @@ package com.example.saglife.screen.forum
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,16 +34,28 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
+import coil.ImageLoader
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.example.saglife.R
 import com.example.saglife.database.getUsernameFromUid
 import com.example.saglife.models.ForumPostItem
@@ -51,6 +73,9 @@ fun ForumPostCard(navController: NavHostController, data: ForumPostItem) {
 
     var showDialog by remember { mutableStateOf(false) }
     var selectedImageUrl by remember { mutableStateOf("") }
+    var rotationDegrees by remember { mutableStateOf(0f) }
+
+
 
     // Récupération des données du post
     val icon = data.icon
@@ -61,7 +86,8 @@ fun ForumPostCard(navController: NavHostController, data: ForumPostItem) {
     val description = data.description
     val imageUrls = data.imageUrls
 
-    println("data imageUrls : $imageUrls")
+    var scale by remember { mutableStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
 
     // Récupération du nom de l'auteur en fonction de son ID
     if (author_id != "") {
@@ -71,21 +97,59 @@ fun ForumPostCard(navController: NavHostController, data: ForumPostItem) {
     }
 
     if (showDialog) {
-        Dialog(onDismissRequest = { showDialog = false }) {
-            // Utilisez Box pour permettre à l'utilisateur de fermer la boîte de dialogue en cliquant en dehors de l'image
-            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-                Image(
-                    painter = rememberAsyncImagePainter(selectedImageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .clickable { showDialog = false },
-                    contentScale = ContentScale.Crop
-                )
+        Dialog(onDismissRequest = { showDialog = false }, properties = DialogProperties(usePlatformDefaultWidth = false)) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()) {
+            Image(
+                painter = rememberAsyncImagePainter(selectedImageUrl),
+                contentDescription = "Image aggrandie du post",
+                modifier = Modifier.fillMaxSize()
+                    .wrapContentHeight()
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y,
+                        rotationZ = rotationDegrees,
+                        transformOrigin = TransformOrigin.Center
+                    )
+                    .pointerInput(Unit) {
+                        detectTransformGestures { _, pan, zoom, _ ->
+                            scale *= zoom
+                            // Ajuster le déplacement en fonction de la rotation de l'image
+                            val adjustedPan = when (rotationDegrees) {
+                                0f, 360f -> pan
+                                90f -> Offset(-pan.y, +pan.x)
+                                180f -> Offset(-pan.x, -pan.y)
+                                270f -> Offset(pan.y, -pan.x)
+                                else -> pan // Pour les rotations non standard
+                            }
+
+                            offset += adjustedPan
+                        }
+                    }
+                    .clickable {
+                        rotationDegrees += if(rotationDegrees == 0f) 90f else -90f
+                    },
+                contentScale = ContentScale.Fit,
+
+            )
+
+                IconButton(onClick = { showDialog = false
+                    scale =1f
+                    offset = Offset.Zero
+                    rotationDegrees = 0f}, modifier = Modifier.align(Alignment.TopEnd).padding(16.dp)) {
+                    Icon(Icons.Default.Close, contentDescription = "Fermer")
+                }
             }
+
         }
+
     }
+
+
 
 
     Card(
